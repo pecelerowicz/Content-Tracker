872c82176d186fefb0571918487f7b7e659d0ac2
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2023-03-01 12:14:22 +0100

    Using InMemoryUserDetailService

---
+++ content-module/src/main/java/com/mpecel/content/tracker/configuration/CustomUser.java
@@ -0,0 +1,57 @@
+package com.mpecel.content.tracker.configuration;
+
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+public class CustomUser implements UserDetails {
+
+    private List<SimpleGrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
+}
